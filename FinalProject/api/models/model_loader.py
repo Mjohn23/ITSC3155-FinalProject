49b4add978fc83fe1b3d12f8d05a456f322ecc93
Karim Sasa <ksasa@uncc.edu>
2024-07-29 18:12:12 -0400

    feat: Updated model_loader.py to include orders, and menu_item

--- FinalProject/api/models/model_loader.py
+++ FinalProject/api/models/model_loader.py
@@ -1,13 +1,8 @@
-from . import orders, order_details, recipes, sandwiches, resources, user , customer
-
+from . import user, customer, orders, menu_item
 from ..dependencies.database import engine
 
-
 def index():
-    orders.Base.metadata.create_all(engine)
-    order_details.Base.metadata.create_all(engine)
-    recipes.Base.metadata.create_all(engine)
-    sandwiches.Base.metadata.create_all(engine)
-    resources.Base.metadata.create_all(engine)
     user.Base.metadata.create_all(engine)
     customer.Base.metadata.create_all(engine)
+    orders.Base.metadata.create_all(engine)
+    menu_item.Base.metadata.create_all(engine)
