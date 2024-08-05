725caeb0ca024a07ab4276c8bf8cfc54b2a53970
Karim Sasa <ksasa@uncc.edu>
2024-08-05 09:12:53 -0400

    feat: small fix to orders.py

--- FinalProject/api/models/orders.py
+++ FinalProject/api/models/orders.py
@@ -9,8 +9,8 @@ class Order(Base):
     order_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     customer_id = Column(Integer, ForeignKey("customers.customer_id"))
     total_amount = Column(DECIMAL, nullable=False)
-    order_status = Column(String(255), nullable=False)  # Specify length for String
-    order_type = Column(String(255), nullable=False)  # Specify length for String
+    order_status = Column(String(255), nullable=False)  
+    order_type = Column(String(255), nullable=False)  
     order_date = Column(DateTime, default=lambda: datetime.now(timezone.utc))
 
     customer = relationship("Customer", back_populates="orders")
